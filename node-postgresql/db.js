f1dbec1edea1af04969938b6ac9c1b9322995706
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2023-12-10 17:24:06 -0300

    atualizando banco

--- node-postgresql/db.js
+++ node-postgresql/db.js
@@ -20,3 +20,44 @@ async function connect(){
 }
 
 connect();
+
+async function selectProdutos(){
+    const client = await connect();
+    const res = await client.query("Select * from produto");
+    return res.rows;
+}
+
+async function selectProduto(id){
+    const client = await connect();
+    const res = await client.query("Select * from produto where id_produto=$1", [id]);
+    return res.rows;
+}
+
+async function insertProduto(produto){
+    const client = await connect();
+    const sql = "insert into produto(nome_produto,preco) values ($1,$2)";
+    const values = [produto.nome_produto, produto.preco];
+    await client.query(sql, values);
+}
+
+async function updateProduto(id_produto, produto){
+    const client = await connect();
+    const sql = "update produto set nome_produto=$1, preco=$2 where id_produto=$3";
+    const values = [produto.nome_produto, produto.preco, id_produto];
+    await client.query(sql, values);
+}
+
+async function deleteProduto(id_produto){
+    const client = await connect();
+    const sql = "delete from produto where id_produto=$1";
+    const values = [id_produto];
+    await client.query(sql, values);
+}
+
+module.exports = {
+    selectProdutos,
+    selectProduto,
+    insertProduto,
+    updateProduto,
+    deleteProduto
+}
--- node-postgresql/index.js
+++ node-postgresql/index.js
@@ -8,10 +8,40 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.json({
         message: "Funcionando!"
     })
+})
+
+app.get("/clientes/:id", async (req, res) => {
+    const cliente = await db.selectProduto(req.params.id);
+    res.json(cliente);
+})
+
+
+app.get("/clientes", async (req, res) => {
+    const clientes = await db.selectProdutos();
+    res.json(clientes);
+})
+
+app.post("/clientes", async (req, res) => {
+    await db.insertProduto(req.body);
+    res.sendStatus(201);
+})
+
+// update no banco
+app.patch("/clientes/:id", async (req, res) => {
+    await db.updateProduto(req.params.id, req.body);
+    res.sendStatus(200);
+})
+
+
+app.delete("/clientes/:id", async (req, res) => {
+    await db.deleteProduto(req.params.id);
+    res.sendStatus(204);
 })
 
 app.listen(port);
