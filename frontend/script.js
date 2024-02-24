0cf70d04b4a9e13bcbe07006bf39528cd3cce751
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-02-24 14:59:52 -0300

    upadate script

--- frontend/script.js
+++ frontend/script.js
@@ -1,55 +1,24 @@
-
-const infoIMg = document.querySelector('.infoIMg')
-const produtosRes = document.getElementById('produtos')
 const listProdutos = document.getElementById('listProdutos')
-const modalDiv = document.querySelector('.modalDiv')
 
-function MostrarProdutos() {
-  
-  fetch('http://localhost:3000/produtos')
-  .then(response => {
-    return response.json()
-  })
-  .then(produtos => {
-    const produtosNaLista = document.querySelector('.produtoNaLista')
-    console.log(produtos)
-    produtos.map((p) => {
-      const div = document.createElement('div')
-      const div2 = document.createElement('div')
-      const div3 = document.createElement('div')
 
-      div.setAttribute('class', "produtoNaLista")
-      div.setAttribute('id', p.id_produto)
-      div.style.display="flex"
 
-     
-      div2.setAttribute('class', "infoProduto")
-      div2.innerText = `Nome: ${p.nome_produto} \n
-      Preço: ${p.preco}`
-
-      div3.setAttribute('class', "infoIMg")
-      const img = document.createElement('img')
-      img.setAttribute('src', "img/delete_FILL0_wght400_GRAD0_opsz24.png")
-      img.setAttribute('class', "imgLixeira")
-      div3.appendChild(img)
-
-      div.appendChild(div2)
-      div.appendChild(div3)
-    
-      listProdutos.appendChild(div)
-
-      
-    })
-
-  })
-  .catch(error => {
-    console.log("erro ao fazer requisição",error)
-  })
-
+async function removerProdutoNoBancoDeDados(produtoId) {
+  try {
+      const response = await fetch(`http://localhost:3000/produtos/${produtoId}`, {
+          method: 'DELETE'
+      });
+      if (response.ok) {
+          console.log(`Produto com ID ${produtoId} removido do banco de dados com sucesso.`);
+      } else {
+          throw new Error("Erro ao excluir produto do banco de dados.");
+      }
+  } catch (error) {
+      console.error("Erro:", error.message);
+      alert("Erro ao excluir produto do banco de dados.");
+  }
+  listProdutos.innerHTML=""
+  MostrarProdutos()
 }
-
-
-MostrarProdutos()
 
 // prettier
 
@@ -89,32 +58,65 @@ document.addEventListener("DOMContentLoaded", function() {
           alert("Erro ao cadastrar produto");
       }
   });
+
 });
 
-
-const imgLixeira = document.getElementById('listProdutos')
-
-imgLixeira.addEventListener('click', async (evt) =>{
-  const produtoId = evt.target.parentNode.parentNode.id
-
-  const resposta = window.confirm("Você deseja excluir esse produto?.");
-
-
- if (resposta) {
-  
-    fetch(`http://localhost:3000/produtos/${produtoId}`, {
-      method: 'DELETE'
-  })
-  .then(response => {
-      if (response.ok) {
-          alert(`Elemento com ID ${produtoId} removido com sucesso.`);
-      } else {
-          alert.error('Erro ao deletar o elemento:', response.statusText);
+listProdutos.addEventListener("click", async function(event) {
+   
+  if (event.target.classList.contains("imgLixeira")) {
+      const produtoId = event.target.parentNode.parentNode.id;
+      // Mostrar mensagem de confirmação
+      const confirmacao = confirm("Tem certeza que deseja excluir este produto?");
+      if (confirmacao) {
+          // Se confirmado, remover o produto no banco de dados
+          await removerProdutoNoBancoDeDados(produtoId);
       }
-      listProdutos.innerHTML=""
-      MostrarProdutos()
+    }
+});
+
+function MostrarProdutos() {
+
+  fetch('http://localhost:3000/produtos')
+  .then(response => {
+    return response.json()
+  })
+  .then(produtos => {
+    console.log(produtos)
+    produtos.map((p) => {
+      const div = document.createElement('div')
+      const div2 = document.createElement('div')
+      const div3 = document.createElement('div')
+
+      div.setAttribute('class', "produtoNaLista")
+      div.setAttribute('id', p.id_produto)
+      div.style.display="flex"
+
+    
+      div2.setAttribute('class', "infoProduto")
+      div2.innerText = `Nome: ${p.nome_produto} \n
+      Preço: ${p.preco}`
+
+      div3.setAttribute('class', "infoIMg")
+      const img = document.createElement('img')
+      img.setAttribute('src', "img/delete_FILL0_wght400_GRAD0_opsz24.png")
+      img.setAttribute('class', "imgLixeira")
+      div3.appendChild(img)
+
+      div.appendChild(div2)
+      div.appendChild(div3)
+    
+      listProdutos.appendChild(div)
+
     })
-    .catch(error => console.error('Erro ao deletar o elemento:', error));
- } 
-  
-})
+
+  })
+  .catch(error => {
+    console.log("erro ao fazer requisição",error)
+  })
+
+}
+
+
+MostrarProdutos()
+
+
